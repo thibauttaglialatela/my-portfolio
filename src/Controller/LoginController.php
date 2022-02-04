c34ffd0dc6fcb1b6622ce25550b499c2d40a454a
thibauttaglialatela <thibauttaglialatela@gmail.com>
2022-02-04 21:51:17 +0100

    create render login form

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -5,16 +5,24 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class LoginController extends AbstractController
 {
     /**
      * @Route("/login", name="login")
      */
-    public function index(): Response
+    public function index(AuthenticationUtils $authenticationUtils): Response
     {
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // Last username entererd by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
         return $this->render('login/index.html.twig', [
-            'controller_name' => 'LoginController',
+            'last_username' => $lastUsername,
+            'error' => $error,
         ]);
     }
 }
