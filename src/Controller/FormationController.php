05a5087a164364da0023af75c964e9d4bce86e82
thibauttaglialatela <thibauttaglialatela@gmail.com>
2022-02-17 16:10:48 +0100

    page d'une formation terminée + merge sur dev

--- src/Controller/FormationController.php
+++ src/Controller/FormationController.php
@@ -2,11 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\Content;
 use App\Repository\CategoryRepository;
 use App\Repository\ContentRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 
 /**
  * @Route("/formation", name="formation_")
@@ -22,6 +24,19 @@ class FormationController extends AbstractController
 
         return $this->render('formation/index.html.twig', [
             'formations' => $contentRepository->findBy(['category' => $catFormation], ['date' => 'DESC'])
+        ]);
+    }
+
+    /**
+     * @Route("/show/{id}", name="show", methods={"GET"})
+     * @ParamConverter("content", class="App\Entity\Content")
+     */
+    public function show(Content $content): Response
+    {
+
+
+        return $this->render('formation/show.html.twig', [
+            'content' => $content,
         ]);
     }
 }
