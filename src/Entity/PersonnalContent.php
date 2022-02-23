431b0441bcbe8d280d8ef5c9f793af32a03b36a0
thibauttaglialatela <thibauttaglialatela@gmail.com>
2022-02-23 09:01:45 +0100

    correction taille images card

--- src/Entity/PersonnalContent.php
+++ src/Entity/PersonnalContent.php
@@ -6,6 +6,7 @@ use App\Repository\PersonnalContentRepository;
 use Doctrine\ORM\Mapping as ORM;
 use DateTime;
 
+//TODO: faire l'upload de ma photo'
 /**
  * @ORM\Entity(repositoryClass=PersonnalContentRepository::class)
  */
