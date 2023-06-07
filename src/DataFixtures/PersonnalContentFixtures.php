a355672db561c8664622a01c2357db1d2359c429
thibauttaglialatela <thibauttaglialatela@gmail.com>
2023-06-07 14:43:44 +0200

    création summary avec les fixtures

--- src/DataFixtures/PersonnalContentFixtures.php
+++ src/DataFixtures/PersonnalContentFixtures.php
@@ -5,17 +5,20 @@ namespace App\DataFixtures;
 use App\Entity\PersonnalContent;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 
 class PersonnalContentFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
+        $faker = Factory::create('fr_FR');
         $perso = new PersonnalContent();
         $perso->setFirstname('Thibaut - Louis');
         $perso->setLastname("Taglialatela");
         $perso->setAdress("impasse Berlioz");
         $perso->setTown("Pau");
         $perso->setZipCode(64000);
+        $perso->setSummary($faker->paragraph());
         $perso->setEmail("thibauttaglialatela@gmai.com");
         $perso->setPhoneNumber("0673592383");
         $perso->setGithubLink("https://github.com/thibauttaglialatela");
